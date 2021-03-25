85188e91cbdaf42577f02d7923dc0e06e582a1fd
Shivam Patel <shivampatel251097@gmail.com>
2021-03-26 01:35:50 +0530

    final

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const smtpTransport = require('nodemailer-smtp-transport');
 var subscribersList = require('./model/subscribersList');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({
