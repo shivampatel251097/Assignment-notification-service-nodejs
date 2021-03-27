cff26ffe5c2c8714ab6fd54ed55957c1b94619dd
Shivam Patel <shivampatel251097@gmail.com>
2021-03-28 02:59:09 +0530

    Text and Wp

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+require('dotenv').config()
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
@@ -9,6 +9,12 @@ const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const smtpTransport = require('nodemailer-smtp-transport');
 var subscribersList = require('./model/subscribersList');
+
+//twilio 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -17,7 +23,7 @@ app.use(express.urlencoded({
 }));
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to Express!")
+  res.send("Welcome to Express!")
 })
 
 app.post('/sendmail',(req,res)=>{
@@ -53,7 +59,7 @@ app.post('/sendmail',(req,res)=>{
     res.send("Mail Sent successfully to all the Subscribers!");
 })
 
-app.post('/schedule',(req,res)=>{
+app.post('/scheduleMail',(req,res)=>{
 
     var reqdata = req.body;
     var dateTime = req.body.dateTime;
@@ -69,9 +75,9 @@ app.post('/schedule',(req,res)=>{
     
 
     const sheduleDate =new Date(year,month,date,hour,min,sec);
-    var zoneTime = sheduleDate.toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
-    new_date = new Date(zoneTime);
-    final_date=new_date.toLocaleString()
+    // var zoneTime = sheduleDate.toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
+    // new_date = new Date(zoneTime);
+    // final_date=new_date.toLocaleString()
     // console.log("year "+year);
     // console.log("month "+month);
     // console.log("date "+date);
@@ -108,8 +114,104 @@ app.post('/schedule',(req,res)=>{
         }
         console.log("Mail Sent to Subscribers!")
     })
-    res.send("Cron job sheduled successfully mail be sent to all the Subscribers at sheduled time!");
+    res.send("Cron job sheduled successfully mail will be sent to all the Subscribers at sheduled time!");
 })
+
+
+
+app.post('/sendtext',(req,res)=>{
+  for(let i=0;i<subscribersList.length;i++){
+    client.messages 
+    .create({ 
+       body: req.body.text,  
+       messagingServiceSid: 'MG6a5291b7397e45776ac5cb092a48114e',      
+       to: subscribersList[i].PhoneNo
+     }) 
+    .then(message => {
+      console.log(message);
+    }) 
+    .done();
+  }
+  res.send("Text message is sent successfully to all the Subscribers!");
+})
+
+app.post('/scheduletext',(req,res)=>{
+
+    var dateTime = req.body.dateTime;
+    var datePart = dateTime.split('T')[0];
+    var date = datePart.split('/')[0];
+    var month = datePart.split('/')[1];
+    var year = datePart.split('/')[2];
+
+    var timePart = dateTime.split('T')[1];
+    var hour = timePart.split(':')[0];
+    var min = timePart.split(':')[1];
+    var sec = timePart.split(':')[2];
+    const sheduleDate =new Date(year,month,date,hour,min,sec);
+  
+    const job = schedule.scheduleJob(sheduleDate, ()=>{
+      for(let i=0;i<subscribersList.length;i++){
+      client.messages 
+      .create({ 
+         body: req.body.text,  
+         messagingServiceSid: 'MG6a5291b7397e45776ac5cb092a48114e',      
+         to: subscribersList[i].PhoneNo
+       }) 
+      .then(message => {
+        console.log(message);
+      }) 
+      .done();
+    }
+  })
+    res.send("Text message will be sent successfully to all the subscribers at sheduled time!");
+  })
+
+app.post('/sendwhatsapp',(req,res)=>{
+  for(let i=0;i<subscribersList.length;i++){
+    client.messages 
+    .create({ 
+       body: req.body.text, 
+       from: 'whatsapp:+14155238886',       
+       to: "whatsapp:"+subscribersList[i].PhoneNo
+     }) 
+    .then(message => {
+      console.log(message);
+    }) 
+    .done();
+}
+res.send("What's App message successfully sent to al the Subscribers!");
+})
+
+
+app.post('/shedulewhatsapp',(req,res)=>{
+  
+    var dateTime = req.body.dateTime;
+    var datePart = dateTime.split('T')[0];
+    var date = datePart.split('/')[0];
+    var month = datePart.split('/')[1];
+    var year = datePart.split('/')[2];
+
+    var timePart = dateTime.split('T')[1];
+    var hour = timePart.split(':')[0];
+    var min = timePart.split(':')[1];
+    var sec = timePart.split(':')[2];
+    const sheduleDate =new Date(year,month,date,hour,min,sec);
+    const job = schedule.scheduleJob(sheduleDate, ()=>{
+      for(let i=0;i<subscribersList.length;i++){
+        client.messages 
+        .create({ 
+           body: req.body.text, 
+           from: 'whatsapp:+14155238886',       
+           to: "whatsapp:"+subscribersList[i].PhoneNo
+         }) 
+        .then(message => {
+          console.log(message);
+        }) 
+        .done();
+    }
+  })
+    res.send("What's App message Will be delivered to all the subscribers on the sheduled time!");
+  })
 
 app.listen(port,()=>{
     console.log(`App is running on port http://localhost:${port}`);
--- model/subscribersList.js
+++ model/subscribersList.js
@@ -1,12 +1,17 @@
 var subscribersList = [
     {
         "name":"Shibbu Patel",
-        "PhoneNo": 7355362257,
+        "PhoneNo": +917355362257,
         "email":"shibbu251097@gmail.com"
     },
     {
         "name":"Shivam Patel",
-        "PhoneNo": 9451776213,
+        "PhoneNo": +919451776213,
+        "email":"shivampatel251097@gmail.com"
+    },
+    {
+        "name":"Test User",
+        "PhoneNo": +916284287839,
         "email":"shivampatel251097@gmail.com"
     }
 ]
