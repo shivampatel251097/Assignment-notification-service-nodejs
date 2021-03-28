1f1a5d805de86faa8080b8cfd59b806daba42659
Shivam Patel <shivampatel251097@gmail.com>
2021-03-28 18:44:01 +0530

    final

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
@@ -17,8 +19,13 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', __dirname + '/public');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
 app.get('/',(req,res)=>{
-  res.send("Welcome to Express!")
+  res.render('index.html');
 })
 
 app.use('/slack',slackRouter);
